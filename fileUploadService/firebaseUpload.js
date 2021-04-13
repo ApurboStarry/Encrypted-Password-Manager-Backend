2aa05137e5dd9ed49c1f4818ac98fff81bb1a2c3
ApurboStarry <hasinapurbo984@gmail.com>
2021-04-13 20:32:17 +0600

    cleaned the firebaseUpload.js file

--- fileUploadService/firebaseUpload.js
+++ fileUploadService/firebaseUpload.js
@@ -1,8 +1,6 @@
-const { rejects } = require("assert");
 const path = require("path");
 const firebase = require("firebase/app");
 const fs = require("fs");
-const { resolve } = require("path");
 require("firebase/storage");
 global.XMLHttpRequest = require("xhr2");
 
@@ -23,28 +21,18 @@ const storageRef = firebase.storage().ref();
 
 async function uploadToFirebase(userId, filename) {
   const fileLocationInFirebase = userId + "/" + filename;
-  console.log("fileLocationInFirebase", fileLocationInFirebase);
   const fileLocationInServer = path.join(__dirname, "../") + "fileUploads/" + userId + filename;
 
-  console.log("fileLocationInServer", fileLocationInServer);
-
   // first upload to firebase
-  const imagesRef = storageRef.child(fileLocationInFirebase); // child("file name to be created")
-  var metadata = {
-    contentType: "application/pdf",
-  };
+  const fileRef = storageRef.child(fileLocationInFirebase); // child("file name to be created")
 
   const fsPromise = await fs.promises.readFile(fileLocationInServer);
-  // console.log("fsPromise", fsPromise);
-  try {
-    const snapshot = await imagesRef.put(fsPromise.buffer, metadata);
-  } catch (error) {
-    console.error(error);
-  }
+  
+  const snapshot = await fileRef.put(fsPromise.buffer);
   console.log("File uploaded");
 
   const url = await storageRef.child(fileLocationInFirebase).getDownloadURL();
-  // console.log(url);
+
   return new Promise((resolve, reject) => {
     resolve(url);
   });
